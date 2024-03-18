fd09713cb7679c87bd923c64f6fa12848612da0d
XenoverseUp <xenoverseup@gmail.com>
2024-03-18 11:02:46 +0300

    feat: preprocessing

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, send_file
+
+
+
+load_dotenv() # os.getenv("SECRET_KEY")
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+    return "Stable."
+
+@app.post("/api/recognize")
+def recognize():
+    data = request.get_json()
+
+    return "Recognized."
+    # return send_file('/temp/0000-0000-0000-0000.musicxml')
+
+@app.post("/api/harmonize")
+def harmonize():
+    data = request.get_json()
+
+    return "Harmonized."
