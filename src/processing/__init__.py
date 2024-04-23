422c054f15444a717c0361d283c61ddf8c2a5ef6
XenoverseUp <xenoverseup@gmail.com>
2024-04-23 14:21:58 +0300

    feat: render musicxml to midi and wave

--- src/processing/__init__.py
+++ src/processing/__init__.py
@@ -1,6 +1,7 @@
 from os import getcwd, path, remove, mkdir, getenv
 from queue import Queue
 from oemer.ete import extract_inline
+from util import musicxml_to_midi, midi_to_wav
 import requests
 
 current_job = []
@@ -17,47 +18,51 @@ def process(save_on_process=False):
             current_job.append(job_id)
             xml = extract_inline(image_path)
 
-
-            # Success
-
-            body = {
-                "user_id": user_id,
-                "job_id": job_id,
-                "xml": xml.decode("utf-8"),
-                "status": "SUCCESS"
-            }
-
-            requests.put(
-                f"{gateway_host}/user/updateSheetStatus",
-                headers={ "Authorization": secret },
-                json=body)
-
             if save_on_process:
                 if not path.exists(path.join(getcwd(), 'out')):
                     mkdir(path.join(getcwd(), 'out'))
 
-                with open(path.join(getcwd(), "out", f"{job_id}.musicxml"), "wb") as file:
+                if not path.exists(path.join(getcwd(), 'out', "musicxml")):
+                    mkdir(path.join(getcwd(), 'out', "musicxml"))
+
+
+                with open(path.join(getcwd(), "out", "musicxml", f"{job_id}.musicxml"), "wb") as file:
                     file.write(xml)
 
+                musicxml_to_midi(job_id)
+
+
+                soundfont = path.join(getcwd(), "src", "assets", "yamaha-c3-grand.sf2")
+                midi_to_wav(job_id, soundfont)
+
+            else:
+                body = {
+                    "user_id": user_id,
+                    "job_id": job_id,
+                    "xml": xml.decode("utf-8"),
+                    "status": "SUCCESS"
+                }
+
+                requests.put(
+                    f"{gateway_host}/user/updateSheetStatus",
+                    headers={ "Authorization": secret },
+                    json=body)
+
         except Exception as err:
-            print(err)
+            if save_on_process: print(err)
+            else:
+                body = {
+                    "user_id": user_id,
+                    "job_id": job_id,
+                    "xml": "",
+                    "status": "FAILED"
+                }
 
-            body = {
-                "user_id": user_id,
-                "job_id": job_id,
-                "xml": "",
-                "status": "FAILED"
-            }
+                requests.put(
+                    f"{gateway_host}/user/updateSheetStatus",
+                    headers={ "Authorization": secret },
+                    json=body)
 
-            requests.put(
-                f"{gateway_host}/user/updateSheetStatus",
-                headers={ "Authorization": secret },
-                json=body)
-            
         finally:
-            remove(image_path)
+            if not save_on_process: remove(image_path)
             current_job.remove(job_id)
-
-
-
-
--- src/util/__init__.py
+++ src/util/__init__.py
@@ -2,6 +2,8 @@ import base64
 import re
 from flask import Response as r, json
 from enum import Enum
+from music21 import converter, midi
+from os import path, getcwd, mkdir, system
 
 def Response(data: dict, status: int = 200):
     return r(json.dumps(data), status=status)
@@ -24,3 +26,34 @@ class Status(Enum):
     PROCESSING = "processing"
     READY = "ready"
     ERROR = "error"
+
+
+def musicxml_to_midi(job_id):
+    print("Building midi file...")
+
+    if not path.exists(path.join(getcwd(), 'out', "midi")):
+        mkdir(path.join(getcwd(), 'out', "midi"))
+
+    xml_file = path.join(getcwd(), "out", "musicxml", f"{job_id}.musicxml")
+    score = converter.parse(xml_file)
+
+    midi_file = path.join(getcwd(), "out", "midi", f"{job_id}.mid")
+    score.write("midi", midi_file)
+
+
+def midi_to_wav(job_id, soundfont):
+    print("Building wav file...")
+
+    if not path.exists(path.join(getcwd(), 'out', "wav")):
+        mkdir(path.join(getcwd(), 'out', "wav"))
+
+    midi_file = path.join(getcwd(), "out", "midi", f"{job_id}.mid")
+    wav_file = path.join(getcwd(), "out", "wav", f"{job_id}.wav")
+
+    try:
+        system(f'fluidsynth -ni {soundfont} {midi_file} -F {wav_file} -r 44100')
+    except:
+        print("Error rendering to wav.")
+
+    
+    
